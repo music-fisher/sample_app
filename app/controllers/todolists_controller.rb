6ffe1eba4b7ed87f4275400d1e97b35ae5c4c48e
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-10-24 09:50:52 +0000

    [Finish]2-7画像投稿機能作成

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -31,6 +31,6 @@ end
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '05a5860fe7a6156124526ad587d2715eee9be841157f126bb4a08532a07ce2c64cebbbd0c7a4e8785327e5983217b6bce548f4e7a3435bc75205c7c56a0dbe49'
