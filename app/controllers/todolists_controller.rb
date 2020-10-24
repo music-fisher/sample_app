588bbd91d915b046111347db56d4dba7f15541ef
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-10-24 13:41:52 +0000

    [Start]2-8削除機能作成

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -27,7 +27,11 @@ def update
   list.update(list_params)
   redirect_to todolist_path(list.id)
 end
-
+def destroy
+  list = List.find(params[:id])
+  list.destroy
+  redirect_to todolists_path#投稿一覧画面へリダイレクト 
+end
   private
   #ストロングパラメータ
   def list_params
